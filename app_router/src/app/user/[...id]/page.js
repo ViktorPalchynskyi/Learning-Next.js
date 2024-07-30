059dacf7d4e9d630c147161a2fa064dabbd99c35
Viktor <palchinskij19@gmail.com>
2024-07-30 10:02:37 +0300

    30.07.24 - added user not found page and used notFound() function

--- app_router/src/app/user/[...id]/page.js
+++ app_router/src/app/user/[...id]/page.js
@@ -1,5 +1,15 @@
+import { notFound } from 'next/navigation';
+
 export default function UserPersonalPage({ params }) {
     const { id } = params;
+
+    if (id.length > 4) {
+        throw new Error('Too many options!');
+    }
+
+    if (id[2] === '1') {
+        notFound();
+    }
 
     return (
         <div>
