2ef105c9bdd7fbae6f3929472b64dbb2523e087e
Viktor <palchinskij19@gmail.com>
2024-08-01 20:28:17 +0300

    01.08.24 - fetching data by server side

--- app_router/src/app/(content)/news/page.js
+++ app_router/src/app/(content)/news/page.js
@@ -1,38 +1,11 @@
-'use client';
+export default async function NewsPage() {
+    const res = await fetch('http://localhost:8080/news');
 
-import { useEffect, useState } from 'react';
-
-export default function NewsPage() {
-    const [news, setNews] = useState([]);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-        const fetchNew = async () => {
-            setLoading(true);
-            const res = await fetch('http://localhost:8080/news');
-            console.log(res);
-            if (!res.ok) {
-                setLoading(false);
-                setError('Fail to load news.');
-            }
-
-            const news = await res.json();
-           
-            setLoading(false);
-            setNews(news);
-        };
-
-        fetchNew();
-    }, []);
-
-    if (loading) {
-        return <p>Loading ...</p>;
+    if (!res.ok) {
+        return <p>Failed to fetch news</p>;
     }
 
-    if (error) {
-        return <p>{error}</p>;
-    }
+    const news = await res.json();
 
     return (
         <div>
