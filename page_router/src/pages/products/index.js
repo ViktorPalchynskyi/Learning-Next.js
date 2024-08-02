60859c121c67d13ffbb20b8fc1ba12901b1ae4da
Viktor <palchinskij19@gmail.com>
2024-08-02 19:33:40 +0300

    02.08.24 - getStaticProps options

--- page_router/src/pages/products/index.js
+++ page_router/src/pages/products/index.js
@@ -1,13 +1,25 @@
 import path from 'node:path';
 import { promises as fs } from 'node:fs';
 
-export async function getStaticProps() {
-    console.log('Re...');
+export async function getStaticProps(context) {
+    console.log('Re...', context);
 
     const filePath = path.join(process.cwd(), '/data/dummy-backend.json');
     const jsonFile = await fs.readFile(filePath);
     const data = JSON.parse(jsonFile);
 
+    if (!data) {
+        return {
+            redirect: {
+                destination: '/'
+            }
+        }
+    }
+
+    if (!data.products.length) {
+        return { notFound: true }
+    }
+ 
     return { props: { products: data.products }, revalidate: 10 };
 }
 
