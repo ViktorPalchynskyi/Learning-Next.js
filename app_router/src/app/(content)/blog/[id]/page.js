bd7585cc1e32188a00a0b0c150d91349169d4caa
Viktor <palchinskij19@gmail.com>
2024-08-02 17:35:28 +0300

    02.08.24 - added a dynamic metadata

--- app_router/src/app/(content)/blog/[id]/page.js
+++ app_router/src/app/(content)/blog/[id]/page.js
@@ -1,6 +1,15 @@
 import Link from 'next/link';
 
-export default function BlogPost({ params }) {
+export async function generateMetadata({ params, number }) {
+    const { id } = params;
+
+    return {
+        title: `A blog post #${id}`,
+        description: `A random blog number is ${Math.round(Math.random() * 100)}`,
+    };
+}
+
+export default async function BlogPost({ params }) {
     const { id } = params;
 
     return (
