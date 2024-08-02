9bac596c8be46750b8d36b9fd6f78224200c870c
Viktor <palchinskij19@gmail.com>
2024-08-02 18:43:48 +0300

    02.08.24 - navigating programmatically

--- page_router/src/pages/user/[...id]/index.js
+++ page_router/src/pages/user/[...id]/index.js
@@ -3,16 +3,20 @@ import { useRouter } from 'next/router';
 export default function UserPersonalPage() {
     const router = useRouter();
     const { id } = router.query;
-  
+
     if (id?.length > 4) {
         throw new Error('Too many options!');
     }
 
+    function handleClick() {
+        router.replace('/');
+    }
 
     return (
         <div>
             <h1>Personal Page</h1>
             <p>Id: {id && id.join('/')}</p>
+            <button onClick={handleClick}>Go home</button>
         </div>
     );
 }
